c654d3d3f2d1a29c1691d6efc83859792f2439b9
Pavel Chekov <chekov_pavel_@mail.ru>
2020-04-13 23:06:14 +0300

    add create farebase

---
+++ Firebase/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Firebase
+//
+//  Created by Pavel Chekov on 13/04/2020.
+//  Copyright © 2020 Pavel Chekov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
