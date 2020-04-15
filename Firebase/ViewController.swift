82118d271aa17e0b903c6302282311775d2e86cf
Pavel Chekov <chekov_pavel_@mail.ru>
2020-04-15 14:14:28 +0300

    udate output/input

--- Firebase/ViewController.swift
+++ Firebase/ViewController.swift
@@ -12,10 +12,10 @@ import FirebaseAuth
 class ViewController: UIViewController {
     
     @IBOutlet weak var passwordTextField: UITextField!
-       
+    
     
     @IBOutlet weak var loginTextFild: UITextField!
-   
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,6 +23,13 @@ class ViewController: UIViewController {
         
         
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        loginTextFild.text = nil
+        passwordTextField.text = nil
+       }
     
     @IBAction func registrationAction(_ sender: Any) {
         if let login = loginTextFild.text, let password = passwordTextField.text  {
@@ -32,16 +39,37 @@ class ViewController: UIViewController {
                     print(error.localizedDescription)
                 }
                 
-                let login = authResult?.user.email
-                print(login)
+                //                let login = authResult?.user.email
+                //                print(login)
+                //
+                //            }
+                
+                if authResult != nil {
+                    self.performSegue(withIdentifier: "showVC", sender: nil )
+                }
+                
                 
             }
-            
+            loginTextFild.text = nil
+            passwordTextField.text = nil
             
         }
     }
     
     @IBAction func signInAction(_ sender: Any) {
+        if let login = loginTextFild.text, let password = passwordTextField.text  {
+        Auth.auth().signIn(withEmail: login, password: password) { [weak self] authResult, error in
+            
+            if let error = error {
+                print(error.localizedDescription)
+            }
+            
+            
+            if authResult != nil {
+                self?.performSegue(withIdentifier: "showVC", sender: nil )
+            }
+        }
     }
+}
 }
 
